a6e8c88c1c9f713dd314b85f64bf7a75100c61eb
Nick Otter <nicholas.a.otter@gmail.com>
2018-08-20 15:09:03 +0100

    refactoring runner.py

--- runner.py
+++ runner.py
@@ -10,28 +10,23 @@ import glob
 from tf_assertion_helper import finder
 
 class Runner(object):
-    """Terraform converter, converting .tf files into JSON and Python"""
+    """
+    Terraform converter, converting .tf files into JSON and Python.
+
+    Pass in .tf file as argument on initialisation.
+    """
 
     def __init__(self, snippet):
         self.snippet = snippet
         self.run()
 
-    def _removetmpdir(self):
-        shutil.rmtree(self.tmpdir)
-
     def _mktmpdir(self):
         self.tmpdir = tempfile.mkdtemp()
-
-    def _terraform_init(self):
-        subprocess.call(["terraform", "init", self.tmpdir])
 
     def _write_test_tf(self):
         tmp_mytf_file = open("%s/mytf.tf" % (self.tmpdir), "w")
         tmp_mytf_file.write(self.snippet)
         tmp_mytf_file.close()
-
-    def _teraform_plan(self):
-        os.system("terraform plan -input=false -out=%s/mytf.tfplan %s" % (self.tmpdir, self.tmpdir))
 
     def _copy_tf_files(self):
         os.system("rm -rf .terraform/modules")
@@ -41,6 +36,25 @@ class Runner(object):
         for file in files:
             if os.path.isfile(file):
                 shutil.copy(file, "%s/mymodule" % (self.tmpdir))
+
+    def _terraform_init(self):
+        subprocess.call(["terraform", "init", self.tmpdir])
+
+    def _teraform_plan(self):
+        os.system("terraform plan -input=false -out=%s/mytf.tfplan %s" % (self.tmpdir, self.tmpdir))
+
+    def snippet_to_json(self):
+        return subprocess.check_output(["tfjson", "%s/mytf.tfplan" % (self.tmpdir)])
+
+    @staticmethod
+    def json_to_dict(json_file):
+        return json.loads(json_file)
+
+    def _removetmpdir(self):
+        shutil.rmtree(self.tmpdir)
+
+    def finder(parent, starts_with, matching_object):
+        return finder(parent, starts_with, matching_object)
 
     def run(self):
         self._mktmpdir()
@@ -52,13 +66,3 @@ class Runner(object):
         result = self.json_to_dict(json_snippet)
         self.result = result
         self._removetmpdir()
-
-    def snippet_to_json(self):
-        return subprocess.check_output(["tfjson", "%s/mytf.tfplan" % (self.tmpdir)])
-
-    @staticmethod
-    def json_to_dict(json_file):
-        return json.loads(json_file)
-
-    def finder(parent, starts_with, matching_object):
-        return finder(parent, starts_with, matching_object)
